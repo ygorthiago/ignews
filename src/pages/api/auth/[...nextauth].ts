30d3392123478ac41d8de1e6e8a62152f9cad761
jenova <ygorthiagoff@outlook.com>
2021-04-15 15:19:58 -0300

    feat: signin callback to create user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query } from 'faunadb';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,39 @@ export default NextAuth({
       scope: 'react:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } },
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+
+    },
+  }
 });
