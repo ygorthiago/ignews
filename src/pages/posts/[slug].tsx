a35583461d6502460aad0c89eb56586b0bd546b1
jenova <ygorthiagoff@outlook.com>
2021-04-16 17:28:58 -0300

    fix: destination url

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,10 +37,10 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: '/home',
+        destination: '/',
         permanent: false,
       }
     }
